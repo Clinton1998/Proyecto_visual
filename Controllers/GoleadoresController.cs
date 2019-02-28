95c6ddd370a8593679f6cdbca8d744991f280e6c
Clinton TL <44932378+Clinton1998@users.noreply.github.com>
2019-02-28 16:32:11 -0500

    Proyecto_de_visual

---
+++ Controllers/GoleadoresController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ejemplo_del.Models;
+
+
+namespace Ejemplo_del.Controllers
+{
+    public class GoleadoresController : Controller
+    {
+        JugadoresModelo=new JugadoresModelo();
+       
+
+    }
+    [ChildActionOnly]
+    public ActionResult _Goleadores()
+    {
+        List<GOLEADORES> lista = this.JugadoresModelo.GetGoleadores();
+        return PartialView(lista);
+
+    }
+
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+
+}
---
+++ Models/JugadoresModelo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejemplo_del.Models
+{
+    public class JugadoresModelo
+    {
+        // GET: JugadoresModels
+        ContextoJugadores contexto;
+        public JugadoresModelo()
+        {
+            contexto = new ContextoJugadores();
+        }
+        public List<GOLEADORES> GetGoleadores()
+        {
+            var consulta = from datos in contexto.GOLEADORES select datos;
+            return consulta.ToList();
+
+        }
+    }
+}
